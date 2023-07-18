d76b42ebef281e28fbd53184812c4c48a9e9d33a
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2023-07-18 21:29:33 +0300

    adding navigate (/Film)} in MovieCard.jsx;  removing <button> and  navigate (/Film) in HomeMovie.jsx

--- src/components/HomeMovie.jsx
+++ src/components/HomeMovie.jsx
@@ -5,20 +5,16 @@ import { movieData } from "../constants/MovieData"
 import { useState } from "react"
 import Add from "./Add"
 import Filter from "./Filter"
-import { useNavigate } from "react-router-dom"
 
 const HomeMovie = () => {
 	const [movies, setMovies] = useState(movieData)
 	const originalMovies = [...movieData]
-	
-	const navigate = useNavigate()
 
 	return (
 		<div className="bg-secondary bg-gradient rounded-4 w-75 my-5 mx-auto text-white" style={{width: "1300px"}}>
 			<Add movies={movies} setMovies={setMovies} />
 			<Filter movies={movies} setMovies={setMovies}  originalMovies={originalMovies}/>
 			<MovieList movies={movies} setMovies={setMovies} />
-			<button onClick={() => navigate ("/Film")}></button>
 		</div>
 	);
 };
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 /* eslint-disable react/prop-types */
 const MovieCard = ( {title, description, posterURL, rating} ) => {
+    const navigate = useNavigate()
+
     return (
-        <div className="border text-center" style={{width: "227px"}}>
+        <div onClick={() => navigate ("/Film")} className="border text-center" style={{width: "227px", cursor: "pointer"}}>
             <img src={posterURL} alt="image"/>
             <h2>{title}</h2>
             <p>{description}</p>
